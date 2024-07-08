1083fba5c5961d682eaa1635877b1c8f5eb44183
khalfi aymen <khalfiaymen.fl@gmail.com>
2024-07-08 23:09:52 +0100

    dropDwon

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,98 @@
+"use client"
+
+import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { eventFormSchema } from '@/lib/validator'
+import * as z from "zod"
+import { eventDefaultValues } from '@/constants'
+import Dropdown from './Dropdown'
+import { Textarea } from '../ui/textarea'
+
+type EventFormProps = {
+    userId: string,
+    type: "Create" | "Update"
+}
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+
+    const form = useForm<z.infer<typeof eventFormSchema>>({
+        resolver: zodResolver(eventFormSchema),
+        defaultValues: eventDefaultValues,
+    })
+
+    function onSubmit(values: z.infer<typeof eventFormSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                <FormControl>
+                                    <Input placeholder="Event title" {...field} className='input-field' />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                <FormControl>
+                                    <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    {/* <FileUploader
+                                        onFieldChange={field.onChange}
+                                        imageUrl={field.value}
+                                        setFiles={setFiles}
+                                    /> */}
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default EventForm
